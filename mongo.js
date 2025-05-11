1a0333993a98eb530ed0ecbce8ab8af47e6789e2
GonzaloHaag <gonzalohaag13@gmail.com>
2025-05-10 23:34:03 -0300

    archive mongo.js add, exercise 12

--- mongo.js
+++ mongo.js
@@ -1,47 +1,46 @@
-/** ARCHIVO DE PRUEBA */
-// const mongoose = require('mongoose');
+/** ARCHIVO DE PRUEBA -- Ej 12 */
+const mongoose = require('mongoose')
+if (process.argv.length < 3) {
+// si no viene la contraseña
+  console.log('give password as argument')
+  process.exit(1)
+}
 
-// if (process.argv.length < 3) {
-//     // si no viene la contraseña
-//     console.log('give password as argument')
-//     process.exit(1)
-// }
+const password = process.argv[2]// viene por terminal cuando corro node mongo.js yourPassword
+const url = `mongodb+srv://fullstack:${password}@cursomongodb.qbsz8.mongodb.net/personsApp?retryWrites=true&w=majority&appName=CursoMongoDB`
 
-// const password = process.argv[2]; // viene por terminal cuando corro node mongo.js yourPassword
-// const url = `mongodb+srv://fullstack:${password}@cursomongodb.qbsz8.mongodb.net/personsApp?retryWrites=true&w=majority&appName=CursoMongoDB`;
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
 
-// mongoose.set('strictQuery', false)
-// mongoose.connect(url)
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
 
-// const personSchema = new mongoose.Schema({
-//     name: String,
-//     number: String,
-// })
+const Person = mongoose.model('Person', personSchema)
 
-// const Person = mongoose.model('Person', personSchema);
-
-// if (process.argv.length === 3) {
+if (process.argv.length === 3) {
 // Significa que solo vino la contraseña al correr el programa
-//     Person.find({}).then((result) => {
-//         result.forEach(person => {
-//             console.log(person.name + ' ' + person.number)
-//         });
+  Person.find({}).then((result) => {
+    result.forEach(person => {
+      console.log(person.name + ' ' + person.number)
+    })
 
-//         mongoose.connection.close()
-//     });
-// } else {
-//     const namePerson = process.argv[3];
-//     const numberPerson = process.argv[4];
-//     const person = new Person({
-//         name: namePerson,
-//         number: numberPerson
-//     });
-//     person.save().then(result => {
-//         console.log(`Added ${result.name} number ${result.number} to phonebook`)
-//         mongoose.connection.close()
-//     });
+    mongoose.connection.close()
+  })
+} else {
+  const namePerson = process.argv[3]
+  const numberPerson = process.argv[4]
+  const person = new Person({
+    name: namePerson,
+    number: numberPerson
+  })
+  person.save().then(result => {
+    console.log(`Added ${result.name} number ${result.number} to phonebook`)
+    mongoose.connection.close()
+  })
 
-// }
+}
 
 
 
